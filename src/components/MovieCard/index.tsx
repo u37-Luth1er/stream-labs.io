c9d50c6151845615af944decdf2b7699be637358
Luth1er <andrei.oliveira99@outlook.com>
2024-11-02 01:25:31 -0300

    Update index.tsx

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -18,12 +18,6 @@ export default function MovieCard(props: Props){
                     src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} 
                     alt={movie.title}/>
             </div>
-            <div className="">
-                <p>
-                    {movie.title}
-                </p>
-            </div>
-
 
             <div className="movie-infos">
                 <p className="movie-title">
