ac92c66e1e9f64e86fcd9affb12ccaccfa2af477
u37-Luth1er <andrei.oliveira99@outlook.com>
2024-11-20 15:24:17 -0300

    updadtes

--- app/pages/dashboard/page.tsx
+++ app/pages/dashboard/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { Suspense } from 'react';
 import MovieList from "@/components/Movielist";
 import LoggedNav from "@/components/LoggedNavBar";
 import PagHyper from "@/components/PaginationHyper";
@@ -6,8 +9,14 @@ export default function DashboardPage() {
   return (
     <div>
       <LoggedNav />
-      <MovieList />
-      <PagHyper />
+      {/* Encapsula MovieList em Suspense se usar hooks do cliente */}
+      <Suspense fallback={<div>Loading movies...</div>}>
+        <MovieList />
+      </Suspense>
+      {/* Encapsula PagHyper em Suspense se usar hooks do cliente */}
+      <Suspense fallback={<div>Loading pagination...</div>}>
+        <PagHyper />
+      </Suspense>
     </div>
   );
 }
